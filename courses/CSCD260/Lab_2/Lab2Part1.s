6ccac5434bfa8f1e838f9ea13801ec08dc702c5c
Isaiah Boone Bessire <153872567+Samson81@users.noreply.github.com>
2025-05-07 16:26:00 -0700

    Comments Added

--- courses/CSCD260/Lab_2/Lab2Part1.s
+++ courses/CSCD260/Lab_2/Lab2Part1.s
@@ -1,11 +1,15 @@
 .global _start
 _start:
 	
-	MOV R0, #0xF
-	MOV R1, #0x19
-	MUL R1, R1, R0
-	MOV R0, #0x145
-	MUL R1, R1, R0
-	MOVW R0, #0xA120
-	MOVT R0, #0x7
-	ADD R0, R0, R1
+    MOV R0, #0xF         @ 15
+    MOV R1, #0x19        @ 2.5 * 10
+    MUL R1, R1, R0
+    MOV R0, #0x145       @ 32.5 * 10
+    MUL R1, R1, R0
+    MOVW R0, #0xA120     @ 5000 * 100
+    MOVT R0, #0x7        @ Top of above line
+    ADD R0, R0, R1
+    
+    @ Reason for the two multiplications by 10 is they get 
+    @ multiplied anyway, and I want to multiply by 100 to get cents
+    @ 15 * 2.5 * 32.5 * 100
